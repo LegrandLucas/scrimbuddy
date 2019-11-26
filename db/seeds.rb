414f92f5d20b6fad3bf312765a6721da9a8746b8
Antoine Massoni <a.massoni93@gmail.com>
2019-11-26 16:02:51 +0100

    pull direct car changement dans les db qu'on avait oublié

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'faker'
+
+puts "Destroy all seeds..."
+
+User.destroy_all
+Review.destroy_all
+Team.destroy_all
+Scrim.destroy_all
+
+puts "Creating users, teams, reviews and scrims..."
+
+fnatic = Team.create!(
+  name: "Fnatic",
+  league: "LEC",
+  country: "England",
+  description: "Fnatic is a professional esports organization headquartered in London, United Kingdom. Founded 23 July 2004, the team has players from around the world, across a variety of games"
+  )
+
+tim_lipovsek = User.create!(
+  first_name: "Tim",
+  last_name: "Lipovsek",
+  email: "tim.lipovsek@fnatic.com",
+  password: "azerty",
+  lol_account: "FNC Nêmesis",
+  pseudo: "Nemesis",
+  team: fnatic,
+  user_type: "player",
+  role: "Mid"
+  )
+
+gabriel_rau = User.create!(
+  first_name: "Gabriel",
+  last_name: "Rau",
+  email: "gabriel.rau@fnatic.com",
+  password: "azerty",
+  lol_account: "FNC Bwipo",
+  pseudo: "Bwipo",
+  team: fnatic,
+  user_type: "player",
+  role: "Top"
+  )
+
+
+puts "Finished!"
+
