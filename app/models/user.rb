28100268af3257bea579fe4e48ceb515d187e1a7
Antoine Massoni <a.massoni93@gmail.com>
2019-11-26 17:10:39 +0100

    seed step by step

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   belongs_to :team
   has_many :reviews, dependent: :destroy
-  has_many :messages, dependent: :destroy
+  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,13 @@ fnatic = Team.create!(
   league: "LEC",
   country: "England",
   description: "Fnatic is a professional esports organization headquartered in London, United Kingdom. Founded 23 July 2004, the team has players from around the world, across a variety of games"
-  )
+)
+image_path = Rails.root.join('app', 'assets', 'images', 'fnatic.png')
+logo_path = Rails.root.join('app', 'assets', 'images', 'logofnatic.png')
+
+fnatic.images.attach(io: File.open(image_path), filename: 'fnatic.png', content_type: 'image/png')
+fnatic.logo.attach(io: File.open(logo_path), filename: 'logofnatic.png', content_type: 'image/png')
+
 
 tim_lipovsek = User.create!(
   first_name: "Tim",
@@ -35,6 +41,9 @@ tim_lipovsek = User.create!(
   role: "Mid"
   )
 
+avatartim_path = Rails.root.join('app', 'assets', 'images', 'tim.png')
+tim_lipovsek.avatar.attach(io: File.open(avatartim_path), filename: 'tim.png', content_type: 'image/png')
+
 gabriel_rau = User.create!(
   first_name: "Gabriel",
   last_name: "Rau",
@@ -46,6 +55,55 @@ gabriel_rau = User.create!(
   user_type: "player",
   role: "Top"
   )
+
+avatargab_path = Rails.root.join('app', 'assets', 'images', 'gab.png')
+tim_lipovsek.avatar.attach(io: File.open(avatargab_path), filename: 'gab.png', content_type: 'image/png')
+
+mads_pedersen = User.create!(
+  first_name: "Mads",
+  last_name: "Brock Pedersen",
+  email: "mads.pedersen@fnatic.com",
+  password: "azerty",
+  lol_account: "FNC Brox",
+  pseudo: "Broxah",
+  team: fnatic,
+  user_type: "player",
+  role: "Jungle"
+  )
+
+zdravets_galabov = User.create!(
+  first_name: "Zdravets",
+  last_name: "Galabov",
+  email: "zdravets.galabov@fnatic.com",
+  password: "azerty",
+  lol_account: "FNC Hylissang",
+  pseudo: "Hylissangh",
+  team: fnatic,
+  user_type: "player",
+  role: "Support"
+  )
+
+martin_larsson = User.create!(
+  first_name: "Zdravets",
+  last_name: "Galabov",
+  email: "martin.larsson@fnatic.com",
+  password: "azerty",
+  lol_account: "Fnatic Rekkles",
+  pseudo: "Rekkles",
+  team: fnatic,
+  user_type: "player",
+  role: "Ad Carry"
+)
+
+louis_victor_legendre = User.create!(
+  first_name: "Louis-Victor",
+  last_name: "Legendre",
+  email: "lv-legendre@fnatic.com",
+  password: "azerty",
+  pseudo: "Mephisto",
+  team: fnatic,
+  user_type: "coach",
+)
 
 
 puts "Finished!"
