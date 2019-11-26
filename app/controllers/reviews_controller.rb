6a3302129ae800af688f9c2723de810be38062c8
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-26 15:25:03 +0100

    first commit review new added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    autorize @review
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
