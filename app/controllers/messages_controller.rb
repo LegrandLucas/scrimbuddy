aacc717fce20d339c5f1900695189b96213e0341
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-26 18:10:08 +0100

    destroy messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,4 +6,10 @@ class MessagesController < ApplicationController
   def show
     @message = Message.find(params[:id])
   end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to message_path(@message)
+  end
 end
