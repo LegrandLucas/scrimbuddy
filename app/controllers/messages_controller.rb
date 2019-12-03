ec141d07d7dac51080e5bf22fb111ec946f52d13
Antoine Massoni <a.massoni93@gmail.com>
2019-12-03 11:19:17 +0100

    ajax messages debug

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,17 +11,17 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @friend           = User.find(params[:user_id])
     @message          = Message.new(message_params)
     @message.sender   = current_user
-    @message.receiver = User.find(params[:user_id])
+    @message.receiver = @friend
+    @messages         = current_user.conversation_with(params[:user_id])
     if @message.save
       respond_to do |format|
-        format.html { redirect_to user_messages_path(@message.receiver) }
+        format.html { redirect_to user_messages_path(@friend) }
         format.js
       end
     else
-      @friend   = User.find(params[:user_id])
-      @messages = current_user.conversation_with(params[:user_id])
       respond_to do |format|
         format.html { render :index }
         format.js
